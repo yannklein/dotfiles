6ec3ad76ab0b55fef9d571acacfb6f928682f3a5
Yann Klein <yann.klein@me.com>
2022-01-24 14:37:27 +0900

    add nvm path

--- _.rb
+++ _.rb
@@ -1,19 +1,18 @@
 require 'io/console'
 begin
   require "colored"
-  require "octokit"
+  require "http"
 rescue LoadError
-  puts "Could not find all needed gems. Please run `bundle install` and retry"
+  puts "Could not find all needed gems. Please run `gem install colored http` and retry"
   exit
 end
 
 require "json"
 require "open-uri"
 
-REQUIRED_RUBY_VERSION = "2.6.3"
+REQUIRED_RUBY_VERSION = "2.6.6"
 REQUIRED_GIT_VERSION = "2.0"
 MINIMUM_AVATAR_SIZE = 2 * 1024
-GITHUB_AUTHORIZATION_NOTE = 'Le Wagon setup check'.freeze
 
 $all_good = true
 
@@ -49,46 +48,26 @@ def check_all
       [ false, "Your default git version is outdated: #{version_tokens.join(".")}"]
     end
   end
-  check("GitHub setup") do
-    begin
-      groups = `ssh -T git@github.com 2>&1`.match(/Hi (?<nickname>.*)! You've successfully authenticated/)
-      git_email = (`git config --global user.email`).chomp
-      nickname = groups["nickname"]
-
-      client = Octokit::Client.new
-      client.login = nickname
-      puts "Please type in your GitHub password (login: '#{nickname}'):"
-      print "> "
-      client.password = STDIN.noecho { |stdin| stdin.gets.chomp }
-      puts "\nThanks. Asking some infos to GitHub..."
-      # TODO(ssaunier): https://github.com/octokit/octokit.rb#two-factor-authentication
-
-      authorization =
-        client.authorizations.find { |a| a[:note] == GITHUB_AUTHORIZATION_NOTE } ||
-        client.create_authorization(
-          scopes: ['user:email'],
-          note: GITHUB_AUTHORIZATION_NOTE)
-      client.access_token = authorization["token"]
-
-      avatar_url = client.user[:avatar_url]
-      emails = client.emails.map { |email| email[:email] }
-      client.delete_authorization(authorization[:id])
-
-      if emails.include?(git_email)
-        content_length = `curl -s -I #{avatar_url} | grep 'Content-Length:'`.strip.gsub("Content-Length: ", "").to_i
-        if content_length >= MINIMUM_AVATAR_SIZE
-          [ true, "GitHub email config is OK. And you have a profile picture 📸"]
-        else
-          [ false, "You don't have any profile picture set.\nIt's important, go to github.com/settings/profile and upload a picture right now."]
-        end
-      else
-        [ false,
-          "Your git email is '#{git_email}' and is not listed in https://github.com/settings/emails\n" +
-          "Run `git config --global user.email george@abitbol.com` to set your git email address. With your own!"]
-      end
-    rescue Octokit::Unauthorized => e
-      puts "Wrong GitHub password, please try again 🙏     Details: #{e.message}"
-      exit 1
+  check("git/Github email matching") do
+    git_email = (`git config --global user.email`).chomp
+    puts "Please go to https://github.com/settings/emails and make sure that"
+    puts "the following email is listed on that page:"
+    puts "👉  #{git_email}"
+    print "Is that the case? (y/n + <Enter>)\n> "
+    response = gets.chomp
+    ok = response.downcase.include?("y")
+    [ ok, ok ? "git email is included in Github emails" : "Add #{git_email} to your GitHub account or update your git global settings" ]
+  end
+  check("GitHub profile picture") do
+    groups = `ssh -T git@github.com 2>&1`.match(/Hi (?<nickname>.*)! You've successfully authenticated/)
+    nickname = groups["nickname"]
+    puts "Your username on GitHub is #{nickname}, checking your profile picture now..."
+    avatar_url = JSON.parse(HTTP.get("https://api.github.com/users/#{nickname}"))['avatar_url']
+    content_length = `curl -s -I #{avatar_url} | grep 'content-length:'`.strip.gsub("content-length: ", "").to_i
+    if content_length >= MINIMUM_AVATAR_SIZE
+      [ true, "Thanks for uploading a GitHub profile picture 📸"]
+    else
+      [ false, "You don't have any profile picture set.\nIt's important, go to github.com/settings/profile and upload a picture *right now*."]
     end
   end
   check("git editor setup") do
@@ -111,7 +90,7 @@ end
 def outro
   if $all_good
     puts ""
-    puts "🚀  Awesome! Your laptop is now ready for 9 weeks of hard work :)".green
+    puts "🚀  Awesome! Your laptop is now ready for weeks of hard work :)".green
     puts "Now it's time to onboard on the Alumni platform 👉  kitt.lewagon.com/onboarding"
   else
     puts ""
--- zshrc
+++ zshrc
@@ -1,12 +1,31 @@
-echo "Hi Yann 🤘"
+hi="\r\rHi Yann"
+
+printf "${hi} 🚀"
+sleep 0.1
+printf "${hi} 👻"
+sleep 0.1
+printf "${hi} 😎"
+sleep 0.1
+printf "${hi} 🎸"
+sleep 0.1
+printf "${hi} 🤘\n"
 
 ZSH=$HOME/.oh-my-zsh
 
 # You can change the theme with another one from https://github.com/robbyrussell/oh-my-zsh/wiki/themes
 ZSH_THEME="robbyrussell"
 
-# Useful oh-my-zsh plugins for Le Wagon bootcamps
-plugins=(gitfast last-working-dir common-aliases sublime zsh-syntax-highlighting history-substring-search pyenv ssh-agent)
+# Useful oh-my-zsh plugins for Le Wagon bootcamps (9/11/21 removed pyenv)
+printf "Wanna let ssh-agent manage your SSH 🕵️ ? "
+read ssh_active
+if ! [ $ssh_active ]
+then
+  plugins=(gitfast last-working-dir common-aliases sublime zsh-syntax-highlighting history-substring-search ssh-agent)
+  echo "Gotcha, leave it to me 🕵️ !"
+else
+  plugins=(gitfast last-working-dir common-aliases sublime zsh-syntax-highlighting history-substring-search)
+  echo "Gotcha, let do it old school 🛹 !"
+fi
 
 # (macOS-only) Prevent Homebrew from reporting - https://github.com/Homebrew/brew/blob/master/share/doc/homebrew/Analytics.md
 export HOMEBREW_NO_ANALYTICS=1
@@ -26,7 +45,8 @@ type -a pyenv > /dev/null && eval "$(pyenv init -)" && eval "$(pyenv virtualenv-
 
 # Load nvm if installed (To manage your Node versions)
 export NVM_DIR="$HOME/.nvm"
-[ -s "/usr/local/opt/nvm/nvm.sh" ] && . "/usr/local/opt/nvm/nvm.sh"
+[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && . "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
 # Rails and Ruby uses the local `bin` folder to store binstubs.
 # So instead of running `bin/rails` like the doc says, just run `rails`
@@ -35,7 +55,8 @@ export PATH="./bin:./node_modules/.bin:${PATH}:/usr/local/sbin"
 
 # Load 'lewagon' virtualenv for the Data Bootcamp. You can comment these 2 lines to disable this behavior.
 export PYENV_VIRTUALENV_DISABLE_PROMPT=1
-pyenv activate lewagon 2>/dev/null && echo "🐍 Loading 'lewagon' virtualenv"
+pyenv activate lewagon 2>/dev/null 
+# echo "🐍 Loading 'lewagon' virtualenv"
 
 # Store your own aliases in the ~/.aliases file and load the here.
 [[ -f "$HOME/.aliases" ]] && source "$HOME/.aliases"
@@ -46,6 +67,11 @@ export LC_ALL=en_US.UTF-8
 
 # Alias for student_picker
 alias whosnext="sh /Users/yann/github/student-picker/whosnext.sh"
+
+# Misc aliases
+alias gst='git status -sb'
+
+alias yanndle="bundle || (gem install bundler && bundle)"
 
 # Path for ANDROID and JAVA
 export ANDROID_SDK_ROOT=/Users/yann/Library/Android/sdk
@@ -60,3 +86,10 @@ export PATH=$ANDROID_SDK/emulator:$ANDROID_SDK/tools:$PATH
 export PYTHONPATH="/Users/yann/code/yannklein/data-challenges/04-Decision-Science:$PYTHONPATH"
 export BUNDLER_EDITOR="'/Applications/Sublime Text.app/Contents/SharedSupport/bin/subl' -a"
 export BUNDLER_EDITOR="'/Applications/Sublime Text.app/Contents/SharedSupport/bin/subl' -a"
+
+# The next line updates PATH for the Google Cloud SDK.
+if [ -f '/Users/yann/Downloads/google-cloud-sdk/path.zsh.inc' ]; then . '/Users/yann/Downloads/google-cloud-sdk/path.zsh.inc'; fi
+
+# The next line enables shell command completion for gcloud.
+if [ -f '/Users/yann/Downloads/google-cloud-sdk/completion.zsh.inc' ]; then . '/Users/yann/Downloads/google-cloud-sdk/completion.zsh.inc'; fi
+[ -s "$NVM_DIR/bash_completion" ] && . "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
